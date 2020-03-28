1a19e54a9d19616d31be008e4e59371a3105113e
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-03-28 17:54:25 +0530

    insert method is created

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,39 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+        private TreeNode root = null;
+        private int max = 0;
+        private int count = 0;
+
+        public TreeNode getRoot() {
+                return root;
+        }
+
+        public void insert(int item) {
+                TreeNode node = new TreeNode(item);
+                if (root == null) {
+                        root = node;
+                } else {
+                        TreeNode temp = root;
+                        TreeNode parent = null;
+                        while (temp != null) {
+                                parent = temp;
+                                if (item < temp.getData()) {
+                                        temp = temp.getLeft();
+                                } else if (item >= temp.getData()) {
+                                        temp = temp.getRight();
+                                }
+                        }
+                        if (item <= parent.getData()) {
+                                parent.setLeft(node);
+                        } else {
+                                parent.setRight(node);
+                        }
+                }
+        }
 }
