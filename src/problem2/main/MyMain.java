df4e136980bf04a1b21cb019a0757c8a403b1499
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-03-28 19:27:38 +0530

    main() method is completed

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -8,6 +8,7 @@ package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 
+import problem1.mybst.MyBinarySearchTree;
 import problem1.node.TreeNode;
 
 // executable class
@@ -27,6 +28,23 @@ public class MyMain {
                         traversePostOrder(currentNode.getRight());
                         System.out.print(currentNode.getData() + " ");
                 }
+        }
+
+        public static void main(String[] args) {
+                MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+                myBinarySearchTree.insert(25);
+                myBinarySearchTree.insert(37);
+                myBinarySearchTree.insert(18);
+                myBinarySearchTree.insert(15);
+                myBinarySearchTree.insert(1);
+
+                System.out.println("Traverse the tree in PreOrder :");
+                traversePreOrder(myBinarySearchTree.getRoot());
+
+                System.out.println();
+
+                System.out.println("Traverse the tree in PostOrder :");
+                traversePostOrder(myBinarySearchTree.getRoot());
         }
 }
 
