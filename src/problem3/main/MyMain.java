7d2c1d75a125455397d44e018cc9fd418e899731
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-03-28 19:02:49 +0530

    main() method is completed

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -7,5 +7,18 @@
 package problem3.main;
 // executable class
 // use problem5.student.Student class to create object of student
+
+import problem3.myqueue.MyPriorityQueue;
+
 public class MyMain {
+        public static void main(String[] args) {
+                MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+                myPriorityQueue.push("arpit", 1);
+                myPriorityQueue.push("raghav", 3);
+                myPriorityQueue.push("shraddha", 0);
+                myPriorityQueue.push("Rahul", 2);
+                String pee = myPriorityQueue.peek();
+                myPriorityQueue.print();
+        }
 }
+
