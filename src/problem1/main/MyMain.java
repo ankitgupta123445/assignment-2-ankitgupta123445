0108961da79fd3529bf0368ef5f1a28b006c682d
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-03-28 18:16:11 +0530

    problem 1 is completed

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,21 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
+                System.out.println("Enter the no of element that you want to enter");
+                int length = sc.nextInt();
+                for (int i = 0; i < length; i++) {
+                        myBinarySearchTree.insert(sc.nextInt());
+                }
+                myBinarySearchTree.traverse(myBinarySearchTree.getRoot());
+        }
 }
+
+
