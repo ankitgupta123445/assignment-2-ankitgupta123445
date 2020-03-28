058452571aff0f6c5d919f4351ab7e20cbdf408b
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-03-28 18:26:10 +0530

    node class is done

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -7,4 +7,40 @@
 package problem3.node;
 
 public class Node {
+        private String data;
+        private Node next = null;
+        private int priority;
+
+        public Node(String data, int priority) {
+                this.data = data;
+                this.priority = priority;
+        }
+
+        public Node(String data) {
+                this.data = data;
+        }
+
+        public String getData() {
+                return data;
+        }
+
+        public void setData(String data) {
+                this.data = data;
+        }
+
+        public Node getNext() {
+                return next;
+        }
+
+        public void setNext(Node next) {
+                this.next = next;
+        }
+
+        public int getPriority() {
+                return priority;
+        }
+
+        public void setPriority(int priority) {
+                this.priority = priority;
+        }
 }
