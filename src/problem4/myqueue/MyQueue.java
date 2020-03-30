27a95ac0781d3910da9bb5f2f52ba62ef4009a5c
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-03-30 20:52:42 +0530

    node class

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -8,4 +8,43 @@ package problem4.myqueue;
 // to create queue to store pre - order successor
 
 public class MyQueue {
+        private int size = 0;
+        private Node head = null;
+        private Node tail = null;
+
+        private Node getNode(int index) {
+                Node response = head;
+                for (int i = 0; i < index && response != null; i++) {
+                        response = response.getNext();
+                }
+                return response;
+        }
+
+        private static class Node<E> {
+                private Node next;
+                private Node previous;
+                private int data;
+
+                public Node(int data) {
+                        this.data = data;
+                }
+
+                public Node(Node<E> next, Node previous, int data) {
+                        this.next = next;
+                        this.previous = previous;
+                        this.data = data;
+                }
+
+                public Node<E> getPrevious() {
+                        return previous;
+                }
+
+                public int getData() {
+                        return data;
+                }
+
+                public Node<E> getNext() {
+                        return next;
+                }
+        }
 }
