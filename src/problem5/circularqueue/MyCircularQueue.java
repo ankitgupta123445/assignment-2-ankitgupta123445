6192d01a611f1682f7156a114108ec789e282984
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-03-30 20:41:42 +0530

    my circular queue

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -6,8 +6,71 @@
  */
 package problem5.circularqueue;
 
+import problem5.student.Student;
+
 //to implement circular queue
 public class MyCircularQueue {
+        private int size;
+        private int front;
+        private int rear;
+        private int maxSize;
+        private Student[] circularQueue;
+
+        public MyCircularQueue(int maxSize) {
+                this.maxSize = maxSize;
+                circularQueue = new Student[maxSize];
+                this.front = 0;
+                this.rear = 0;
+                this.size = 0;
+        }
+
+        public int getSize() {
+                return size;
+        }
+
+        public void setSize(int size) {
+                this.size = size;
+        }
+
+        public int getFront() {
+                return front;
+        }
+
+        public void setFront(int front) {
+                this.front = front;
+        }
+
+        public int getRear() {
+                return rear;
+        }
+
+        public void setRear(int rear) {
+                this.rear = rear;
+        }
+
+        public int getMaxSize() {
+                return maxSize;
+        }
+
+        public void setMaxSize(int maxSize) {
+                this.maxSize = maxSize;
+        }
+
+        public Student[] getCircularQueue() {
+                return circularQueue;
+        }
+
+        public void setCircularQueue(Student[] circularQueue) {
+                this.circularQueue = circularQueue;
+        }
+
+        private boolean isEmpty() {
+                return size == 0;
+        }
+
+        private boolean isFull() {
+                return size == maxSize;
+        }
 
 
 }
