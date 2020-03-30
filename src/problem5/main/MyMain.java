98d67fc9eb4b4bd60ea909a2d5f012e56c75df47
ankitgupta123445 <ankit.gla_cs18@gla.ac.in>
2020-03-30 20:49:19 +0530

    done

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -6,9 +6,23 @@
  */
 package problem5.main;
 
+import problem5.circularqueue.MyCircularQueue;
+import problem5.student.Student;
+
 //executable class
 public class MyMain {
         public static void main(String[] args) {
+                MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+                myCircularQueue.add(new Student("deepesh", 0, 0));
+                myCircularQueue.add(new Student("Yashdeep", 1, 1));
+                myCircularQueue.add(new Student("Deeraj", 1, 0));
+                for (int i = 0; i < myCircularQueue.getSize() + 1; i++) {
+                        Student s = myCircularQueue.remove();
+                        if (s.getBackLog() - s.getAppearingCount() != 0) {
+                                myCircularQueue.add(s);
+                        }
+                }
+                myCircularQueue.show();
 
         }
 }
